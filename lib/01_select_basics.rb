b0ef4108ea06e831032d3fc3a8aeaf15267223da
kevalshah598 <kevalshah598@gmail.com>
2023-02-07 15:11:07 -0500

    update

--- lib/01_select_basics.rb
+++ lib/01_select_basics.rb
@@ -36,7 +36,14 @@ def per_capita_gdp
   # Show the name and per capita gdp (gdp/population) for each country where
   # the area is over 5,000,000 km^2
   execute(<<-SQL)
-    
+    SELECT 
+      name, (gdp/population)
+    FROM 
+      countries
+    GROUP BY
+      name
+    HAVING
+      area > 5000000;
   SQL
 end
 
@@ -44,6 +51,14 @@ def small_and_wealthy
   # Show the name and continent of countries where the area is less than 2,000
   # and the gdp is more than 5,000,000,000.
   execute(<<-SQL)
+    SELECT 
+      name, continent
+    FROM 
+      countries
+    GROUP BY 
+      name
+    HAVING
+      gdp > 5000000000 AND area < 2000;
   SQL
 end
 
@@ -51,12 +66,20 @@ def scandinavia
   # Show the name and the population for 'Denmark', 'Finland', 'Norway', and
   # 'Sweden'
   execute(<<-SQL)
+    SELECT
+      name, population
+    FROM
+      countries
+    WHERE 
+      name IN ('Denmark', 'Finland', 'Norway', 'Sweden');
   SQL
 end
 
 def starts_with_g
   # Show each country that begins with the letter G
   execute(<<-SQL)
+    SELECT
+      name
   SQL
 end
 
